e66a9bcd2387f3ebdbb6fcf665f5e71fd085e1b1
Viktoriia Moskalenko <vika01mos@gmail.com>
2023-04-20 22:50:44 +0300

    mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { MONGODB_URI, PORT = 3000 } = process.env;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log("Server running. Use our API on port: " + PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("Database connection error: " + error.message);
+    process.exit(1);
+  });
